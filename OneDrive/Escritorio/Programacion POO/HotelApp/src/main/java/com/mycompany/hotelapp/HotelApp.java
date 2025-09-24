15d08a304c4896503ea0cc869cb12cdadda4e257
Christian Jair <christian.gomez0831@alumnos.udg.mx>
2025-09-24 00:03:12 -0600

    Primer commit

---
+++ OneDrive/Escritorio/Programacion POO/HotelApp/src/main/java/com/mycompany/hotelapp/HotelApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelapp;
+
+/**
+ *
+ * @author chris
+ */
+public class HotelApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
