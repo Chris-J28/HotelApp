9a1e5df671021134367ea89d21de1a61d95dc13c
Emiliano Gutierrez Nuñez <emiliano.gutierrez0923@alumnos.udg.mx>
2025-09-26 21:51:10 -0600

    Update Suite.java

--- OneDrive/Escritorio/Programacion POO/HotelApp/src/main/java/com/mycompany/hotelapp/Suite.java
+++ OneDrive/Escritorio/Programacion POO/HotelApp/src/main/java/com/mycompany/hotelapp/Suite.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.hotelapp;
 
 /**
  *
- * @author chris
+ * @author Emili
  */
-public class Suite {
+public class Suite extends Habitacion {
     
+    private boolean tieneJacuzzi;
+    private boolean tieneVistaMar;
+
+    // Constructor
+    public Suite(int numHabi, double precioNoche, boolean reservacion, boolean tieneJacuzzi, boolean tieneVistaMar) {
+        super(numHabi, precioNoche, reservacion);
+        this.tieneJacuzzi = tieneJacuzzi;
+        this.tieneVistaMar = tieneVistaMar;
+    }
+
+    @Override
+    public String mostrarInfo() {
+        return "--- Suite #" + numHabi + " ---\n" +
+               "Precio por noche: $" + String.format("%.2f", precioNoche) + "\n" +
+               "Estado: " + (reservacion ? "Ocupada" : "Libre") + "\n" +
+               "Jacuzzi: " + (tieneJacuzzi ? "Si" : "No") + "\n" +
+               "Vista al mar: " + (tieneVistaMar ? "Si" : "No");
+    }
 }
